6a423181d9b23c540a447428955ad22531251e80
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-08-18 22:07:06 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,10 +1,12 @@
 # Make your shoe class here!
 class Shoe
-attr_accessor :brand, :color, :size, :material, :condition  
+  attr_accessor :brand, :color, :size, :material, :condition
 
   def initialize(name)
   end
 
-
+  def cobble
+    puts "shoe has been repaired"
+  end
 
 end
